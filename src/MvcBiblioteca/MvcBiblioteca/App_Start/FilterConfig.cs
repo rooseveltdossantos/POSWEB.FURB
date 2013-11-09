5e7a58d94316b0adc46d91705d2efef37974d4d6
rodrigomuller <rodrigo_blu@yahoo.com.br>
2013-11-09 17:29:51 -0200

    Somente entra na aplicação com senha.

--- src/MvcBiblioteca/MvcBiblioteca/App_Start/FilterConfig.cs
+++ src/MvcBiblioteca/MvcBiblioteca/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace MvcBiblioteca
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new AuthorizeAttribute());
             filters.Add(new HandleErrorAttribute());
         }
     }
