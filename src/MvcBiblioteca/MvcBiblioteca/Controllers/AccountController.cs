3a59fcde129db5288f19d390a70fe1ceac579bcd
joaoluizsk <jlsk25@gmail.com>
2013-11-09 17:06:07 -0200

    Ajustado para que somente usuarios que podem comentar são criados.

--- src/MvcBiblioteca/MvcBiblioteca/Controllers/AccountController.cs
+++ src/MvcBiblioteca/MvcBiblioteca/Controllers/AccountController.cs
@@ -80,6 +80,7 @@ namespace MvcBiblioteca.Controllers
                 try
                 {
                     WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
+                    Roles.AddUserToRole(model.UserName, "PodeComentar");
                     WebSecurity.Login(model.UserName, model.Password);
                     return RedirectToAction("Index", "Home");
                 }
