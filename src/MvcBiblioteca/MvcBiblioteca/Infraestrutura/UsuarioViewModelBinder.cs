2b4fbc1130856e7db65f400606d1e559e41e20a3
rooseveltdossantos <rooseveltdossantos@hotmailcom>
2013-11-23 10:26:34 -0200

    Criada infraestrutura para conversão do tipo do usuário.

--- src/MvcBiblioteca/MvcBiblioteca/Infraestrutura/UsuarioViewModelBinder.cs
+++ src/MvcBiblioteca/MvcBiblioteca/Infraestrutura/UsuarioViewModelBinder.cs
@@ -5,11 +5,31 @@ using System.Text;
 using System.Web.Mvc;
 using Biblioteca.DataAccess;
 using MvcBiblioteca.Models;
+using Biblioteca.Dominio;
 
 namespace MvcBiblioteca.Infraestrutura
 {
     public class UsuarioViewModelBinder : DefaultModelBinder
     {
+        protected override void SetProperty(ControllerContext controllerContext, ModelBindingContext bindingContext, System.ComponentModel.PropertyDescriptor propertyDescriptor, object value)
+        {
+            if (propertyDescriptor.PropertyType == typeof(TipoUsuario))
+            {
+                var propertyName = propertyDescriptor.Name;
+                var propertyValue = bindingContext.ModelState[propertyName].Value.AttemptedValue;
+
+                ConverterParaTipoUsuario(bindingContext, propertyName, propertyValue);
+            }
+            else
+                base.SetProperty(controllerContext, bindingContext, propertyDescriptor, value);
+        }
+
+        private void ConverterParaTipoUsuario(ModelBindingContext bindingContext, string propertyName, string propertyValue)
+        {
+            throw new NotImplementedException();
+        }
+
+
         public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
             var valor = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
