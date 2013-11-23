a470a25d8a4e65332b37a908edbbc0d70f96e361
alebae <alebae@gmail.com>
2013-11-23 16:48:50 -0200

    Binder dos livro

---
+++ src/MvcBiblioteca/MvcBiblioteca/Infraestrutura/LivroBinderProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBiblioteca.Models;
+using Biblioteca.Dominio;
+
+namespace MvcBiblioteca.Infraestrutura
+{
+    public class LivroBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(Type modelType)
+        {
+            if (modelType.IsAssignableFrom(typeof(Livro)))
+                return new LivroBinder();
+            return null;
+        }
+    }
+}
