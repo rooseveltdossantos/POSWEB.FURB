4bc572ef9083918957bf0972bad6bfc84d1a3bdc
sandro.bugmann <sandro.bugmann@hotmail.com.br>
2013-11-23 16:17:04 -0200

    Alterada classe Estudante. Novos campos.

--- src/MvcBiblioteca/ExemploEntity.Dominio/Estudante.cs
+++ src/MvcBiblioteca/ExemploEntity.Dominio/Estudante.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DominioEscolar
 {
+    [Table("EstudantesInfo")]
     public class Estudante
     {
-        public Estudante()
-        {
+        public Estudante() { }
 
-        }
-        public int EstudanteId { get; set; }
+        [Key]
+        public int SID { get; set; }
+
+        [Column("Nome", TypeName = "ntext")]
+        [MaxLength(20)]
         public string Nome { get; set; }
+
+        [NotMapped]
+        public int? Age { get; set; }
+
+        public int GrauId { get; set; }
+
+        [ForeignKey("GrauId")]
+        public virtual GrauDeConhecimento GrauDeConhecimento { get; set; }
 
     }
 }
