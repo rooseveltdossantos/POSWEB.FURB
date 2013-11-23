4f8d2c463703660703133b5905822474e081ba6b
arjbnu <arjbnu@gmail.com>
2013-11-23 11:50:17 -0200

    Adicionada classe Estudante

---
+++ src/MvcBiblioteca/ExemploEntity.Dominio/Estudante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DominioEscolar
+{
+    public class Estudante
+    {
+        public Estudante()
+        {
+
+        }
+        public int EstudanteId { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
