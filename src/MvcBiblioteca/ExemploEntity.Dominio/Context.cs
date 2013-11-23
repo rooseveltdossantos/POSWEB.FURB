41c50aa49a7e0d46b09f70528b8fca5786edc3fe
lobokoch@gmail.com <lobokoch@gmail.com>
2013-11-23 11:42:07 -0200

    Criando a classe de contexto

---
+++ src/MvcBiblioteca/ExemploEntity.Dominio/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+
+namespace ExemploEntity.Dominio
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base()
+        {
+
+        }
+
+        public DbSet<Estudante> Estudantes { get; set; }
+        public DbSet<GrauDeConhecimento> GrausDeConhecimento { get; set; }
+
+    }
+
+}
