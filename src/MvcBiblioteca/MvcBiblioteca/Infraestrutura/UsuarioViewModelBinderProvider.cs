b4674240d4f34ac18f6c781dc9521e643ab19edd
rooseveltdossantos <rooseveltdossantos@hotmailcom>
2013-11-23 09:19:09 -0200

    Criado o model binder provider

---
+++ src/MvcBiblioteca/MvcBiblioteca/Infraestrutura/UsuarioViewModelBinderProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBiblioteca.Models;
+
+namespace MvcBiblioteca.Infraestrutura
+{
+    public class UsuarioViewModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(Type modelType)
+        {
+            if (modelType.IsAssignableFrom(typeof(UsuarioViewModel)))
+                return new UsuarioViewModelBinder();
+            return null;
+        }
+    }
+}
