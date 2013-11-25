3420cb8abd4c077265182ffd39720c3f657eec17
Angel Vitor Lopes <vitorbq@gmail.com>
2013-11-25 13:26:14 -0200

    Nova Classe ReservaLivro.cs
    Criada a classe para armazenar as reservas de livros. <i>(OBS: A princípio, quando existir esse registro, o livro foi reservado para o usuário, quando ocorrer o emprestimo do livro, deletar esse registro ?verificar o funcionamento dessa lógica?)</i>

---
+++ src/MvcBiblioteca/Biblioteca.Dominio/ReservaLivro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Biblioteca.Dominio
+{
+    public class ReservaLivro
+    {
+        public int ReservaId { get; set; }
+
+        [DisplayName("Livro")]
+        public Livro LivroRelacionado { get; set; }
+
+        [DisplayName("Usuário")]
+        public Usuario UsuarioDeb { get; set; }
+
+    }
+}
