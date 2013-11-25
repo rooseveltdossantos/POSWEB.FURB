a3dfbce6edfca945d5fa3e1b6086f37fa39d43cb
JCZingel <janainac_25201@hotmail.com>
2013-11-25 00:23:52 -0200

    Criação da classe com atributos para lançamento de débito.

---
+++ src/MvcBiblioteca/Biblioteca.Dominio/Debito.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Biblioteca.Dominio
+{
+    public class Debito
+    {
+        public int DebitoId { get; set; }
+
+        [DisplayName("Usuário")]
+        public Usuario UsuarioDeb { get; set; }
+
+        [DisplayName("Livro")]
+        public Livro LivroRelacionado { get; set; }
+
+        [DisplayName("Data Débito")]
+        public DateTime DataDebito { get; set; }
+
+        [DisplayName("Dias Atraso")]
+        public int DiasAtraso { get; set; }
+
+        public bool DebitoAtivo { get; set; }
+
+    }
+}
