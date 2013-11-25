84b65edd0f76a5b4693bd82e6be96b770c282e06
laheinzen <laheinzen@gmail.com>
2013-11-25 10:54:43 -0200

    Controller Devolucao
    Criado controller da Devolução

---
+++ src/MvcBiblioteca/MvcBiblioteca/Controllers/DevolucaoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBiblioteca.Controllers
+{
+    public class DevolucaoController : Controller
+    {
+        //
+        // GET: /Devolucao/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
