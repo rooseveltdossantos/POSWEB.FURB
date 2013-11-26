2f58047bf6f6884b3108a51379bcbfea34adbafb
rodrigomuller <rodrigo_blu@yahoo.com.br>
2013-11-26 09:19:10 -0200

    Adicionada a classe Emprestimo no Dominio da Aplicação.

---
+++ src/MvcBiblioteca/Biblioteca.Dominio/Emprestimo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Biblioteca.Dominio
+{
+    public class Emprestimo
+    {
+        public int EmprestimoId { get; set; }
+
+        [DisplayName("Livro")]
+        public Livro LivroEmprestimo { get; set; }
+
+        [DisplayName("Usuário")]
+        public Usuario UsuarioEmprestimo { get; set; }
+
+        [DisplayName("Horário de Início")]
+        public DateTime horarioInicio { get; set; }
+
+        [DisplayName("Horário de Término")]
+        public DateTime horarioTermino { get; set; }
+    }
+}
