0d23731d539395420b661f6ba97eb0035946ffc1
JCZingel <janainac_25201@hotmail.com>
2013-11-26 19:20:57 -0200

    Adicionando métodos de pesquisa de débitos ativos ao controller.

--- src/MvcBiblioteca/MvcBiblioteca/Controllers/DebitosController.cs
+++ src/MvcBiblioteca/MvcBiblioteca/Controllers/DebitosController.cs
@@ -3,17 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Biblioteca.DataAccess;
+using Biblioteca.Dominio;
+using MvcBiblioteca.Models;
 
 namespace MvcBiblioteca.Controllers
 {
     public class DebitosController : Controller
     {
-        //
-        // GET: /Debitos/
-
+        /// <summary>
+        /// Método que lista todos os débitos ativos
+        /// </summary>
         public ActionResult Index()
         {
-            return View();
+            return View("Index", ObterDebitos(string.Empty));
+        }
+
+        public ActionResult LocalizarDebitosUsuario(string NomeUsuario)
+        {
+            var debitos = ObterDebitos(NomeUsuario);
+            return View("Index", debitos);
+        }
+
+        private static IEnumerable<Debito> ObterDebitos(string NomeUsuario)
+        {
+            NomeUsuario = NomeUsuario.ToLower();
+
+            using (var bd = new BibliotecaDatabase())
+            {
+                IQueryable<Debito> debitos = from debito in bd.Debitos
+                                             where debito.DebitoAtivo
+                                             orderby debito.Emprestimo.horarioTermino descending
+                                             select debito;
+
+                if (!string.IsNullOrEmpty(NomeUsuario))
+                    debitos = debitos.Where(deb => deb.UsuarioDeb.Nome.ToLower().IndexOf(NomeUsuario) >= 0);
+
+                return debitos.ToList();
+            }
         }
 
     }
