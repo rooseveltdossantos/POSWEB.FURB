524cda6a9851f2a312addefb2339a932e6a90d67
rooseveltdossantos <rooseveltdossantos@hotmail.com>
2013-11-27 08:15:56 -0200

    Testes para exemplificação do Join.

---
+++ src/MvcBiblioteca/MVCBibliotecaTestes/AcessoDadosExemplosTeste.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Biblioteca.DataAccess;
+using NUnit.Framework;
+
+namespace MVCBibliotecaTestes
+{
+    [TestFixture]
+    public class AcessoDadosExemplosTeste
+    {
+       [Test]
+       public void TesteComJoin()
+       {
+           using (var bd = new BibliotecaDatabase())
+           {
+               var q = from l in bd.Livros
+                       join r in bd.Reservas
+                       on l equals r.LivroRelacionado
+                       where r.UsuarioDeb.UsuarioId == 1
+                       select l;
+
+               var livro = q.ToList();
+               livro.ForEach( l => Console.WriteLine(l.Titulo));
+           }
+       }
+    }
+}
