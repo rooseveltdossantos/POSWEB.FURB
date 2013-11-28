ba36f2e143cd787a1f57ca93d3dec5d77658c0e4
saddambea <saddamm@gmail.com>
2013-11-28 20:45:00 -0200

    Commit que devia ter ido ontem!

--- src/MvcBiblioteca/MvcBiblioteca/HtmlHelperBibliotecaExtensions.cs
+++ src/MvcBiblioteca/MvcBiblioteca/HtmlHelperBibliotecaExtensions.cs
@@ -16,29 +16,53 @@ namespace MvcBiblioteca
         {
             var items = Enum.GetValues(typeof(TProperty));
 
-            var nomeDosItems = ((TProperty[])items).Select((p, s) => 
+            var nomeDosItems = ((TProperty[])items).Select((p, s) =>
             {
                 Type tipo = p.GetType();
                 MemberInfo membro = tipo.GetMember(p.ToString())[0];
-                
+
                 var visualizacaoAttribute = (VisualizacaoAttribute)
                         Attribute.GetCustomAttribute(membro, typeof(VisualizacaoAttribute));
 
                 if (visualizacaoAttribute != null)
                     return apresentar.Compile().Invoke(visualizacaoAttribute);
                 else
-                    return p.ToString();                
+                    return p.ToString();
             });
 
-            var selectList = new SelectList(nomeDosItems);
-            return htmlHelper.DropDownListFor(expression, selectList);                       
+            var valorDosItens = ((TProperty[])items).Select((p, s) =>
+            {
+                Type tipo = p.GetType();
+                return p.ToString();
+            });
+
+            //var selectList = new SelectList(nomeDosItems);
+            int contador = 0;
+            //foreach(SelectListItem sli in selectList) 
+            List<SelectListItem> itens = new List<SelectListItem>();
+
+            //crio a tag Select com os itens com cada valor correspondente no enum - verificar
+            foreach (String nome in nomeDosItems)
+            {
+                itens.Add(new SelectListItem
+                {
+                    Selected = false,
+                    Text = nome,
+                    Value = valorDosItens.ToArray()[contador]
+                });
+
+                //sli.Value = valorDosItens.ToArray()[contador];
+                contador++;
+            }
+            return htmlHelper.DropDownListFor(expression, itens); //selectList);                       
         }
 
 
 
-        public static MvcHtmlString LabelFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, Expression<Func<VisualizacaoAttribute, string>> apresentar) {
+        public static MvcHtmlString LabelFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, Expression<Func<VisualizacaoAttribute, string>> apresentar)
+        {
             Type tipoModelo = typeof(TModel);
-            
+
             MemberInfo membro = tipoModelo.GetMember(expression.ReturnType.Name)[0];
 
             var visualizacaoAttribute = (VisualizacaoAttribute)
@@ -47,7 +71,7 @@ namespace MvcBiblioteca
             if (visualizacaoAttribute != null)
                 return htmlHelper.LabelFor(expression, apresentar.Compile().Invoke(visualizacaoAttribute));
             else
-                return htmlHelper.LabelFor(expression);            
+                return htmlHelper.LabelFor(expression);
         }
     }
 }
