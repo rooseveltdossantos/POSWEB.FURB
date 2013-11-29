6434cee4f98ee107fed9d3afe91eb18a2a2b7761
laheinzen <laheinzen@gmail.com>
2013-11-28 23:13:28 -0200

    Criando dados para testes

--- src/MvcBiblioteca/MvcBiblioteca/Controllers/DevolucaoController.cs
+++ src/MvcBiblioteca/MvcBiblioteca/Controllers/DevolucaoController.cs
@@ -13,6 +13,56 @@ namespace MvcBiblioteca.Controllers
     public class DevolucaoController : Controller
     {
 
+        public ActionResult Popular()
+        {
+            var meuUser = new Usuario { Login = "lah", Nome = "Luiz Angelo Heinzen", Senha = "segredo", StatusAtivacao = StatusAtivacao.Ativo, TipoUsuario = TipoUsuario.Funcionario };
+            var proto = new Usuario { Login = "proto", Nome = "Proto(u)suario", Senha = "segredo", StatusAtivacao = StatusAtivacao.Ativo, TipoUsuario = TipoUsuario.Aluno };
+            var tioBill = new Usuario { Login = "bgates", Nome = "", Senha = "segredo", StatusAtivacao = StatusAtivacao.Ativo, TipoUsuario = TipoUsuario.Professor};
+            var guerraDosTronos = new Livro { Ano = 2000, Autor = "George R.R. Martin", Preco = 40m, Titulo = "A Guerra dos Tronos" };
+            var carrie = new Livro { Ano = 1979, Autor = "Stepen King", Preco = 15m, Titulo = "Carrie, a estranha" };
+            var arteGuerra = new Livro { Ano = 1600, Autor = "Sun-Tzu", Preco = 5m, Titulo = "A arte da Guerra" };
+            var jogosVorazes = new Livro { Ano = 2003, Autor = "Stephanie Meyer", Preco = 25m, Titulo = "Jogos Vorazes" };
+            var demian = new Livro { Ano = 1985, Autor = "Herman Hesse", Preco = 8m, Titulo = "Demian" };
+
+            //Primeiro usuarios e livros
+            using (var bd = new BibliotecaDatabase())
+            {
+                if (bd.Usuarios.Count() == 0)
+                {
+                    bd.Usuarios.Add(meuUser) ;
+                    bd.Usuarios.Add(proto);
+                    bd.Usuarios.Add(tioBill);
+                    bd.Livros.Add(guerraDosTronos);
+                    bd.Livros.Add(carrie);
+                    bd.Livros.Add(arteGuerra);
+                    bd.Livros.Add(jogosVorazes);
+                    bd.Livros.Add(demian);
+                    bd.SaveChanges();
+                }
+            }
+
+            var hoje = DateTime.Now.Date;
+            var ontem = hoje.AddDays(-1);
+            var anteOntem = hoje.AddDays(-2);
+            var semanaPassada = hoje.AddDays(-7);
+            var amanha = hoje.AddDays(1);
+            var depoisDeAmanha = hoje.AddDays(2);
+            var proximaSemana = hoje.AddDays(7);
+
+            using(var bd = new BibliotecaDatabase())
+	        {
+                bd.Emprestimos.Add(new Emprestimo { LivroEmprestimo = jogosVorazes, UsuarioEmprestimo = meuUser, RetiradoEm = semanaPassada, DevolverAte = hoje });
+                bd.Emprestimos.Add(new Emprestimo { LivroEmprestimo = carrie, UsuarioEmprestimo = meuUser, RetiradoEm = semanaPassada, DevolverAte = ontem });
+                bd.Emprestimos.Add(new Emprestimo { LivroEmprestimo = guerraDosTronos, UsuarioEmprestimo = meuUser, RetiradoEm = ontem, DevolverAte = proximaSemana });
+                bd.Emprestimos.Add(new Emprestimo { LivroEmprestimo = arteGuerra, UsuarioEmprestimo = tioBill, RetiradoEm = semanaPassada, DevolverAte = proximaSemana });
+	        }
+
+            //Depois os Emprestimos para testar
+
+
+            return View(new DevolucaoViewModel());
+        }
+        
         public ActionResult Index()
         {
             return View(new DevolucaoViewModel());
