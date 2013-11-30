e38b719d920084607d91639a68daaa67101737e1
laheinzen <laheinzen@gmail.com>
2013-11-30 10:22:22 -0200

    Resolvendo Issue 10
    Except está usando referência para comparar.
    Sobrescrevendo métodos na Classe Livro pra resolver.

--- src/MvcBiblioteca/Biblioteca.Dominio/Livro.cs
+++ src/MvcBiblioteca/Biblioteca.Dominio/Livro.cs
@@ -21,5 +21,64 @@ namespace Biblioteca.Dominio
         public decimal Preco { get; set; }
 
         public ICollection<ComentarioLivro> Comentarios { get; set; }
-    }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Livro);
+        }
+
+        public bool Equals(Livro outro)
+            {
+                // If parameter is null, return false. 
+                if (Object.ReferenceEquals(outro, null))
+                {
+                    return false;
+                }
+
+                // Optimization for a common success case. 
+                if (Object.ReferenceEquals(this, outro))
+                {
+                    return true;
+                }
+
+                // If run-time types are not exactly the same, return false. 
+                if (this.GetType() != outro.GetType())
+                    return false;
+
+                // Return true if the fields match. 
+                // Note that the base class is not invoked because it is 
+                // System.Object, which defines Equals as reference equality. 
+                return (outro.LivroId == this.LivroId);
+            }
+
+            public override int GetHashCode()
+            {
+                return this.LivroId * 347;;
+            }
+
+            public static bool operator ==(Livro lhs, Livro rhs)
+            {
+                // Check for null on left side. 
+                if (Object.ReferenceEquals(lhs, null))
+                {
+                    if (Object.ReferenceEquals(rhs, null))
+                    {
+                        // null == null = true. 
+                        return true;
+                    }
+
+                    // Only the left side is null. 
+                    return false;
+                }
+                // Equals handles case of null on right side. 
+                return lhs.Equals(rhs);
+            }
+
+            public static bool operator !=(Livro lhs, Livro rhs)
+            {
+                return !(lhs == rhs);
+            }
+        }
+
+ 
 }
